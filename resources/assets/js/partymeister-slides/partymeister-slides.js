3b167f967e5b63d89911edea3742ef692791f0ad
Reza Esmaili <github@dfox.info>
2019-06-13 16:55:30 +0200

    Replace dom2image with dom2imagemore to fix firefox png generator issues

--- resources/assets/js/partymeister-slides/partymeister-slides.js
+++ resources/assets/js/partymeister-slides/partymeister-slides.js
@@ -1,7 +1,6 @@
 // Slidemeister
 require('./slidemeister/jquery.ui.rotatable');
-// var domtoimage = require('dom-to-image-more');
-window.domtoimage = require('./slidemeister/dom-to-image2');
+window.domtoimage = require('dom-to-image-more');
 require('./slidemeister/slidemeister');
 require('./slidemeister/slidemeister.properties');
 window.MediumEditor = require('medium-editor');
